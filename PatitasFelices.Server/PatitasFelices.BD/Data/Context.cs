803339b662bf4bc3551b4dcf16315b1724f6e7f2
Leandro Bertero <leandro.bertero@hotmail.com>
2025-04-01 00:03:40 -0300

    primera parte: conexion con la base de datos, inluyendo el proyecto server y el proyecto de biblioteca de clases de de entity framework

---
+++ PatitasFelices.Server/PatitasFelices.BD/Data/Context.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatitasFelices.BD.Data
+{
+    public class Context : DbContext
+    {
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected Context( Context contex)
+        {
+        }
+    }
+}
