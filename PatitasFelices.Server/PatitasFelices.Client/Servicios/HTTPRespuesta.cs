d3d3d31e5d69ae08ff291072bb8a8e435b861bf4
Leandro Bertero <leandro.bertero@hotmail.com>
2025-04-19 23:35:34 -0300

    Servicios HTTP

---
+++ PatitasFelices.Server/PatitasFelices.Client/Servicios/HTTPRespuesta.cs
@@ -0,0 +1,19 @@
+﻿namespace PatitasFelices.Client.Servicios
+{
+    public class HTTPRespuesta<T>
+    {
+        public T respuesta { get; }
+
+        public bool Error { get; }
+
+        public HttpResponseMessage httpResponseMessage { get; set; }
+
+        public HTTPRespuesta(T respuesta, bool error, HttpResponseMessage httpResponseMessage)
+        {
+            this.respuesta = respuesta;
+            Error = error;
+            this.httpResponseMessage = httpResponseMessage;
+        }
+
+    }
+}
