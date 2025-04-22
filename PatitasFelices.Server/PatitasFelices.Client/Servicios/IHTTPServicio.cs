f3ebd3f11fbd5b98067f59520d3a116536f36c5f
Leandro Bertero <leandro.bertero@hotmail.com>
2025-04-22 12:00:45 -0300

    Crear comentario con campos incluidos y funcionl, navegacion funcional entre botones redireccionables . (No funcionn ls cargas)

--- PatitasFelices.Server/PatitasFelices.Client/Servicios/IHTTPServicio.cs
+++ PatitasFelices.Server/PatitasFelices.Client/Servicios/IHTTPServicio.cs
@@ -4,5 +4,6 @@ namespace PatitasFelices.Client.Servicios
     public interface IHTTPServicio
     {
         Task<HTTPRespuesta<T>> Get<T>(string url);
+        Task<HTTPRespuesta<object>> Post<T>(string url, T entidad);
     }
 }
